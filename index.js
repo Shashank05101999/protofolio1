30ae63c83ff5ccd6682026d43abe92b44ec5d700
Shashank05101999 <shashankyadav05101999@gmail.com>
2024-01-15 20:10:40 +0530

    existing repo update

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var icon= document.getElementById("icon")
+icon.onclick =function(){
+    document.body.classList.toggle("darkmode")
+    if(document.body.classList.contains("darkmode")){
+        icon.src="sun.jpg"
+    }else
+    {
+        icon.src="m3.png"
+    }
+}
