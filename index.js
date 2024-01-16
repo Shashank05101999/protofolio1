59c309b0f0749d3af2cf841fe18bbc81e6867866
Shashank05101999 <shashankyadav05101999@gmail.com>
2024-01-16 10:27:01 +0530

    Add  new comment

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-var icon= document.getElementById("icon")
-icon.onclick =function(){
-    document.body.classList.toggle("darkmode")
-    if(document.body.classList.contains("darkmode")){
-        icon.src="sun.jpg"
-    }else
-    {
-        icon.src="m3.png"
-    }
-}
-// hello
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("darkmode");
+  if (document.body.classList.contains("darkmode")) {
+    icon.src = "sun.jpg";
+  } else {
+    icon.src = "m3.png";
+  }
+};
+// hello  what is going on
